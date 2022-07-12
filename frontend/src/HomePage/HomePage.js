e32892a0a0414bba8be885df63dd51415de2ff20
Fer Alvarez <feeerr95@gmail.com>
2022-07-11 21:35:01 -0300

    Agrego funcionamiento para mostrar el resultado

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -13,33 +13,60 @@ const initialErrors = [
   'Pieza a medio hacer',
   'Ausencia de filamento al inicio de la impresión',
 ];
+const INITIAL_TITLE = '¿CUÁL ES SU PROBLEMA ACTUAL?';
 
 export const HomePage = () => {
   const [errors, setErrors] = useState(initialErrors);
   const [selectedOption, setSelectedOption] = useState('');
+  const [response, setResponse] = useState('')
+  const [title, setTitle] = useState(INITIAL_TITLE)
 
   const handleNext = async () => {
-    const newErrors = await getNewErrors(selectedOption);
+    const {response, newErrors} = await getNewErrors(selectedOption);
     setErrors(newErrors);
+    setTitle('OBSERVA O REALIZÓ ALGUNA DE LAS SIGUIENTES OPCIONES')
+    setResponse(response)
+  }
+
+  const handleReset = () => {
+    setErrors(initialErrors);
+    setResponse('');
+    setTitle(INITIAL_TITLE);
   }
 
   return (
     <div className="page">
       <span className="t-stroke t-shadow pageTitle">3D ERRORS</span>
-      {/* <span className="pageTitle">3D-ERRORS</span> */}
       <div className="card">
-        <span className="title">Indique cual es el error que presenta.</span>
-        <div className="form">
-          <span className="subtitle">Seleccione alguna de las siguientes opciones.</span>
-          <Form errors={errors} onSelect={setSelectedOption} />
-        </div>
-        <Button 
-          onClick={handleNext}
-          className="button"
-          variant="contained"
-        >
-          Siguiente
-        </Button>
+        {response 
+        ? (
+          <>
+            <span className="title">SOLUCIÓN / PROBLEMA DE SU ERROR</span>
+            <span>{response}</span>
+            <Button 
+              onClick={handleReset}
+              className="button"
+              variant="contained"
+            >
+              Reiniciar
+            </Button>
+          </>
+        ) : (
+          <>
+            <span className="title">{title}</span>
+            <div className="form">
+              <span className="subtitle">Seleccione alguna de las siguientes opciones.</span>
+              <Form errors={errors} onSelect={setSelectedOption} />
+            </div>
+            <Button 
+              onClick={handleNext}
+              className="button"
+              variant="contained"
+            >
+              Siguiente
+            </Button>
+          </>
+        )}
       </div>
     </div>
   )
